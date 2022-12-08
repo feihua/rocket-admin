b6387cd36cb2087a91500cd72d2cbcb789a87d0a
koobe <koobe.liu@uaf.com.cn>
2022-12-08 10:39:16 +0800

    修改访问端口

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,13 @@ pub mod model;
 pub mod vo;
 pub mod utils;
 
+use std::net::Ipv4Addr;
 use std::sync::Arc;
 use rocket::serde::json::serde_json::json;
 use rocket::serde::json::Value;
 use crate::handler::{menu_handler, role_handler, user_handler};
 use rbatis::rbatis::Rbatis;
+use rocket::Config;
 
 #[get("/ping")]
 fn ping() -> &'static str {
@@ -40,7 +42,14 @@ async fn main() -> Result<(), rocket::Error> {
     RB.init(rbdc_mysql::driver::MysqlDriver {}, "mysql://root:r-wz9wop62956dh5k9ed@rm-wz9a2yv489d123yqkdo.mysql.rds.aliyuncs.com:3306/zero-react").unwrap();
     let rb = Arc::new(&RB);
 
+    let config = Config {
+        address: Ipv4Addr::new(0, 0, 0, 0).into(),
+        port: 8099,
+        ..Config::debug_default()
+    };
+
     let _rocket = rocket::build()
+        .configure(config)
         .mount("/", routes![ping])
         .mount("/api", routes![user_handler::login,
             user_handler::query_user_menu,
