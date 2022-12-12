866ea21053e2e2ff3b963234003adb51081f476a
koobe <koobe.liu@uaf.com.cn>
2022-12-12 10:21:03 +0800

    Dockerfile文件调整

--- deployment.sh
+++ deployment.sh
@@ -7,16 +7,16 @@ docker stop rocket-admin
 
 
 #删除容器
-docker rm rocket-admin
+docker rm -f rocket-admin
 
 #删除镜像
-docker rmi rocket-admin:v1
+docker rmi -f rocket-admin:v1
 
 #删除none镜像
-docker rmi $(docker images | grep "none" | awk '{print $3}')
+docker rmi -f $(docker images | grep "none" | awk '{print $3}')
 
 #构建服务
-docker build -t rocket-admin:v1 -f docs/Dockerfile .
+docker build -t rocket-admin:v1 -f Dockerfile .
 
 #启动服务
 docker run -itd --net=host --name=rocket-admin rocket-admin:v1
